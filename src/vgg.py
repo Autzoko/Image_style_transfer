3dde48024ba687ac4d2c2fd0485fe9267e2e0564
Autzoko <gszhai@bjtu.edu.cn>
2022-09-27 19:55:07 +0800

    Self constructed VGG class

--- src/vgg.py
+++ src/vgg.py
@@ -5,4 +5,6 @@ import torch
 import torch.nn as nn
 
 
-
+class Conv(nn.Module):
+	def __init__(self, input_channels, output_channels, kernel_size=1, stride=1, padding=None, groups=1, activation=True):
+		super(Conv, self).__init__()
